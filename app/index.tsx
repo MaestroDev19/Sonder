3efd21ddc499033d49ec30d15e9ae700c0a22a9f
Charles <charles19rex@gmail.com>
2024-06-13 11:17:06 +0100

    onboarding started

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import HomeScreen from './screens/homeScreen';
-import { Link } from 'expo-router';
-
-const App = () => {
+import { Link } from "expo-router";
+import { ArrowLeft, ArrowRight } from "lucide-react-native";
+import React from "react";
+import { View, Dimensions, Text } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+interface SliderInfo {
+  id: number;
+  heading: string;
+  body: string;
+}
+const content = [
+  {
+    id: 1,
+    heading: "Make Music Friends",
+    body: "Connect with people who share your musical soul...",
+  },
+  {
+    id: 2,
+    heading: "Find Your Vibe Tribe",
+    body: "Discover a community of music lovers just like you...",
+  },
+  {
+    id: 3,
+    heading: "Spark a Connection",
+    body: "Maybe even find your musical soulmate!",
+  },
+];
+export default function Onboarding() {
+  const [homeState, setHomeState] = React.useState(false);
+  if (!homeState) {
     return (
-        <>
-            {/* <HomeScreen /> */}
-            <Link style={{paddingTop: 300}} href='/screens/homeScreen'>home</Link>
-        </>
+      <AppIntroSlider
+        data={content}
+        renderItem={({ item }: { item: SliderInfo }) => {
+          return (
+            <View className="flex flex-1 justify-center items-start p-5 space-y-2">
+              <Text className="text-white text-3xl">{item.heading}</Text>
+              <Text className="text-white text-base w-2/3">{item.body}</Text>
+            </View>
+          );
+        }}
+        onDone={() => {
+          setHomeState(true);
+        }}
+      />
     );
-};
-
-export default App;
+  }
+  return (
+    <View className={"flex flex-1 justify-center items-center"}>
+      <Text className="text-white">
+        <Link href={"/screens/homeScreen"}>Home</Link>
+      </Text>
+    </View>
+  );
+}
--- assets/svg/exports.ts
+++ assets/svg/exports.ts
@@ -1,4 +1,4 @@
-export {default as ProfileIcon} from './profile.svg'
-export {default as ChatIcon} from './chats.svg'
-export {default as SettingsIcon} from './settings.svg'
-export {default as ArrowRight} from './arrow-left.svg'
+export { default as ProfileIcon } from "./profile.svg";
+export { default as ChatIcon } from "./chats.svg";
+export { default as SettingsIcon } from "./settings.svg";
+export { default as ArrowRight } from "./arrow-left.svg";
