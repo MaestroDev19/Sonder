c23b1dc82410a7e0f23d9c126407f33dcd1e20be
fortune710 <fortune0208@yahoo.com>
2024-06-27 11:41:18 -0400

    added home page

--- constants/DrawerItems.tsx
+++ constants/DrawerItems.tsx
@@ -1,7 +1,26 @@
 import { View, Text, Image } from "react-native";
 import * as icons from '../assets/svg/exports';
+import { ArrowRight, Home } from "lucide-react-native";
 
 export default [
+    {
+        name: "home/index",
+        options: {
+            drawerLabel: () => (
+            <View className="flex-row items-center w-full">
+                <Text className="text-[#EFEFEF]">
+                    Home
+                </Text>
+                <View className='w-6 h-6 rounded-md border-[#EFEFEF33] bg-[#1DB954] p-1.5 items-center justify-center absolute left-36'>
+                    <ArrowRight size="14px" stroke="#000"/>
+                </View>
+            </View>
+            )
+  ,
+            title: 'Home',
+
+        }
+    },
     {
         name: "profile/index",
         options: {
@@ -10,11 +29,9 @@ export default [
                     <Text className="text-[#EFEFEF]">
                         Profile
                     </Text>
-                    {/* <View className="absolute right-0"> */}
-                        <View className='w-6 h-6 rounded-md border-[#EFEFEF33] bg-[#1DB954] p-1.5 items-center justify-center absolute left-36'>
-                            <Image source={icons.ArrowRight} />
-                        </View>
-                    {/* </View> */}
+                    <View className='w-6 h-6 rounded-md border-[#EFEFEF33] bg-[#1DB954] p-1.5 items-center justify-center absolute left-36'>
+                        <ArrowRight size="14px" stroke="#000"/>
+                    </View>
                 </View>,
             title: 'Profile',
         }
