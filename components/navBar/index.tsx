5c218406d7573c02e4f8e844d8e7cbd71a1524c8
fortune710 <fortune0208@yahoo.com>
2024-07-01 12:16:29 -0400

    added skeleton loading

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -1,40 +1,38 @@
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-// import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity, View } from 'react-native';
 import Avatar from '../avatar';
-import {iconContainer } from './styles';
 import { useNavigation } from 'expo-router';
 import { DrawerActions } from '@react-navigation/native';
 import { Bell } from 'lucide-react-native';
 import useCurrentUser from '../../hooks/current-user';
-import { StyleSheet } from 'nativewind';
+import { Skeleton } from '../skeleton';
 
 
 export function getHomeNavbarOptions() {
-    const navigation = useNavigation('/(screens)')
+  const navigation = useNavigation('/(screens)')
 
-    const { userProfile } = useCurrentUser();
+  const { userProfile, isLoading } = useCurrentUser();
 
-    return {
-      header: () => (
-        <View className='flex items-center justify-between flex-row w-screen px-4 py-5'>
+  return {
+    header: () => (
+      <View className='mt-10 flex items-center justify-between flex-row w-screen px-4 py-5'>
+        {
+          isLoading ? <Skeleton borderRadius={999} width={50} height={50}/> :
           <TouchableOpacity  onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
-            <Avatar width={50} height={50} className='h-10 w-10' src={userProfile?.profile_image || ""} initials="S" />
+            <Avatar 
+              width={50} 
+              height={50} 
+              className='h-10 w-10' 
+              src={userProfile?.profile_image || ""} 
+              initials={userProfile?.name.at(0) || "S"}
+            />
           </TouchableOpacity>
-          
-          <TouchableOpacity className="bg-[#EFEFEF1A] p-3 rounded-xl border border-muted">
-            <Bell stroke="white"/>
-          </TouchableOpacity>
-        </View>
-      )
-    };
+        }
+        
+        <TouchableOpacity className="bg-[#EFEFEF1A] p-3 rounded-xl border border-muted">
+          <Bell stroke="white"/>
+        </TouchableOpacity>
+      </View>
+    )
+  };
 }
-
-const styles = StyleSheet.create({
-  header: {
-    width: "100%",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingTop: 30
-  }
-})
