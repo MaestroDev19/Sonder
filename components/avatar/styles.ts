b05fd53d8f9e469481d4f9a21199e250e870cd29
fortune710 <fortune0208@yahoo.com>
2024-07-01 13:43:20 -0400

    changed avatar styles

--- components/avatar/styles.ts
+++ components/avatar/styles.ts
@@ -1,6 +1,6 @@
 
-const containerStyles = "relative flex h-[100px] w-[100px] shrink-0 justify-center align-center overflow-hidden rounded-full"
+const containerStyles = "relative flex shrink-0 justify-center align-center rounded-full"
 const imageStyles = "aspect-square h-14 w-14"
-const initialsStyles = "flex h-24 w-14 justify-center items-center rounded-full bg-primary"
+const initialsStyles = "flex flex-row justify-center items-center rounded-full bg-primary"
 
 export default { containerStyles, imageStyles, initialsStyles }
