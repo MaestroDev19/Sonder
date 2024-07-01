282313e4d1c8ca3e9ca28912653df59d89f47b5b
fortune710 <fortune0208@yahoo.com>
2024-07-01 12:37:38 -0400

    added skeleton

--- app/(screens)/home/index.tsx
+++ app/(screens)/home/index.tsx
@@ -1,123 +1,40 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Animated, View, Dimensions, Pressable,Text, ViewStyle, FlatList } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { getHomeNavbarOptions } from '../../../components/navBar';
 import NowPlaying from '../../../components/nowPlaying';
 import { container } from './styles';
 import { Drawer } from 'expo-router/drawer';
-import { useNavigation } from 'expo-router';
-import ProfileCard from '../../../components/swipeCards';
-import { ProfileCardProps } from '../../../types/types';
-import { profileCards } from '../../../constants/profileCardsTemplate';
-import { PanGestureHandler } from 'react-native-gesture-handler';
-import { PanResponder } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
-import Page from '../../../components/page';
 import useCurrentTrack from '../../../hooks/current-song';
+import { SwipeCardsContainer } from '../../../components/swipeCardsContainer';
+import { millisecondsToMSFormat } from '../../../utils/functions';
+//import useCurrentUser from '../../../hooks/current-user';
 
-function millisecondsToMSFormat(milliseconds: number): string {
-    // Calculate minutes and seconds from milliseconds
-    const minutes = Math.floor(milliseconds / (1000 * 60)); // Minutes
-    const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000); // Seconds
-  
-    // Format minutes and seconds with leading zeros
-    const formattedMinutes = minutes.toString().padStart(2, '0');
-    const formattedSeconds = seconds.toString().padStart(2, '0');
-  
-    // Combine minutes and seconds with colon separator
-    return `${formattedMinutes}:${formattedSeconds}`;
-}
+
 
 const HomeScreen: React.FC = () => {
-    const navigation = useNavigation();
-    const width = Dimensions.get('window').width;
-    const height = Dimensions.get('window').height;
-    const pan = useRef(new Animated.Value(0)).current;
-    const carouselRef = useRef(null);
+  const { isLoading, currentTrack, currentTrackProgress } = useCurrentTrack();
+  //const { refreshUser } = useCurrentUser()
 
-      
-    const panResponder = useRef(
-      PanResponder.create({
-        onMoveShouldSetPanResponder: () => true,
-        onPanResponderMove: Animated.event(
-          [
-            null,
-            { dy: pan }
-          ],
-          { useNativeDriver: false }
-        ),
-        onPanResponderRelease: (e, gestureState) => {
-          const { dy } = gestureState;
-          if (Math.abs(dy) > 100) {
-            Animated.timing(pan, {
-              toValue: dy > 0 ? height : -height,
-              duration: 300,
-              useNativeDriver: true,
-            }).start(() => {
-              dy > 0 ? carouselRef.current.prev() : carouselRef.current.next() // Reset position for potential reuse
-              pan.setValue(0);
-            });
-          } else {
-            Animated.spring(pan, {
-              toValue: 0,
-              useNativeDriver: true,
-            }).start();
-          }
-        },
-      })
-    ).current;
-  
+  return (
+    <View className={container}>
+      <Drawer.Screen options={getHomeNavbarOptions()} />
 
+      <View className='px-4'>
+        <NowPlaying
+          isLoading={isLoading}
+          songName={currentTrack?.name || "--"}
+          albumArtUrl={currentTrack?.image || 'https://upload.wikimedia.org/wikipedia/en/3/32/Frank_Ocean-Nostalgia_Ultra.jpeg'} //Change default to green image
+          artist={currentTrack?.artists.join(', ') || '--'}
+          timestamp={millisecondsToMSFormat(currentTrackProgress)}
+          device={currentTrack?.device.name || "N/A"}
+          deviceType={currentTrack?.device.type || "Computer"}
+        />
+      </View>
 
-    const { isLoading, currentTrack, refreshTrack, currentTrackProgress } = useCurrentTrack();
+      <SwipeCardsContainer/>
 
-    return (
-        <View className={container}>
-            <Drawer.Screen options={getHomeNavbarOptions()} />
-
-            <View className='px-4'>
-                <NowPlaying
-                    songName={currentTrack?.name || "The Last Year"}
-                    albumArtUrl={currentTrack?.image || 'https://upload.wikimedia.org/wikipedia/en/3/32/Frank_Ocean-Nostalgia_Ultra.jpeg'}
-                    artist={currentTrack?.artists.join(', ') || 'Jessica Pratt'}
-                    timestamp={millisecondsToMSFormat(currentTrackProgress)}
-                    device={currentTrack?.device.name || "Your Device"}
-                />
-            </View>
-              <Animated.View
-                style={{
-                  transform: [{ translateY: pan }],
-                }}
-                {...panResponder.panHandlers}
-              >
-                <Carousel
-                  ref={carouselRef}
-                  loop
-                  width={width}
-                  height={height}
-                  enabled={false}
-                  autoPlay={false}
-                  data={profileCards}
-                  scrollAnimationDuration={1000}
-                  onSnapToItem={(index) => console.log('current index:', index)}
-                  renderItem={renderProfileCard}
-                  />
-              </Animated.View>
-
-            </View>
-    );
-  };
-  const renderProfileCard = ({ item }: { item: ProfileCardProps }) => (
-      <ProfileCard
-          headerImage={item.headerImage}
-          avatar={item.avatar}
-          avatarInitials={item.avatarInitials}
-          userName={item.userName}
-          description={item.description}
-          likedArtist={item.likedArtist}
-          // likedGenre={item.likedGenre}
-          favoriteSong={item.favoriteSong}
-          favoriteArtist={item.favoriteArtist}
-      />
-  )
+    </View>
+  );
+};
 
 export default HomeScreen;
