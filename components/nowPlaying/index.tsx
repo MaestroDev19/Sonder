a7a8b42613a0220da495ba1afac8d4058245d231
fortune710 <fortune0208@yahoo.com>
2024-07-01 12:37:59 -0400

    added skeleton loading

--- components/nowPlaying/index.tsx
+++ components/nowPlaying/index.tsx
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet } from 'react-native';
 import styles from './styles'
 import { Image } from 'expo-image';
 import { scale, verticalScale } from 'react-native-size-matters';
+import { Computer, Laptop } from 'lucide-react-native';
+import { Skeleton } from '../skeleton';
 
 interface NowPlayingProps {
     songName: string;
@@ -10,6 +12,8 @@ interface NowPlayingProps {
     albumArtUrl: string;
     timestamp: string;
     device: string;
+    deviceType: string,
+    isLoading: boolean
 }
 
 const row = "flex flex-row items-center justify-between mb-2"
@@ -21,13 +25,40 @@ const timestamp = "text-sm text-[#EFEFEF80]"
 const device = "text-sm text-white"
 const divider = "h-px  mb-2"
 
+const SKELETON_HEIGHT = 15
+
 const NowPlaying: React.FC<NowPlayingProps> = ({
     songName,
     artist,
     albumArtUrl,
     timestamp,
     device,
+    deviceType,
+    isLoading
 }) => {
+
+    const renderIcon = {
+        'computer': <Laptop stroke="#1DB954"/>
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.container}>
+                <View style={styles.row}>
+                    <View className='flex flex-col w-[60%] gap-3'>
+                        <Skeleton width={"100%"} height={SKELETON_HEIGHT}/>
+                        <Skeleton width={"60%"} height={SKELETON_HEIGHT}/>
+                    </View>
+                    <Skeleton width={scale(40)} height={verticalScale(40)} borderRadius={5}/>
+                </View>
+                <View style={styles.divider}/>
+                <View style={styles.row}>
+                    <Skeleton width={"60%"} height={SKELETON_HEIGHT}/>
+                    <Skeleton width={"10%"} height={SKELETON_HEIGHT}/>
+                </View>
+            </View>
+        )
+    }
 
     return (
         <View style={styles.container}>
@@ -40,7 +71,10 @@ const NowPlaying: React.FC<NowPlayingProps> = ({
             </View>
             <View style={styles.divider}></View>
             <View style={styles.row}>
-                <Text className="text-white font-semibold">{device}</Text>
+                <View className='flex items-center flex-row gap-1'>
+                    {renderIcon[deviceType.toLowerCase()]}
+                    <Text className="text-white font-semibold">{device}</Text>
+                </View>
                 <Text style={styles.text}>{timestamp}</Text>
             </View>
         </View>
