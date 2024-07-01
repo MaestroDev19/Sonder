50fa90d0791e02cd2e8c083c113ccb593db3a28e
fortune710 <fortune0208@yahoo.com>
2024-07-01 13:43:55 -0400

    added skeleton

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -11,6 +11,7 @@ import Avatar from "../avatar";
 import { DrawerContentScrollView, DrawerItemList, DrawerItem } from "@react-navigation/drawer";
 import useCurrentUser from "../../hooks/current-user";
 import { useState, useCallback } from "react";
+import { Skeleton } from "../skeleton";
   
   const CustomDrawer = (props) => {
     const { userProfile, isLoading, refreshUser } = useCurrentUser();
@@ -33,34 +34,46 @@ import { useState, useCallback } from "react";
           contentContainerStyle={{ marginHorizontal: 15 }}
           className="bg-background pt-[65px] h-full"
           refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
-        >
+        >   
+          {
+            isLoading ? <Skeleton width={100} height={100} borderRadius={999} /> :
             <Avatar 
               src={userProfile?.profile_image} 
               initials={userProfile?.name.at(0)?.toUpperCase() || "S"} 
             />
-            <View className="mt-3 text-white">
-                <TouchableOpacity>
-                    <Text className="text-3xl capitalize font-bold text-white">
-                        { userProfile?.name }
-                    </Text>
-                    <Text className="text-grey-text font-semibold">
-                        @{userProfile?.spotify_username}
-                    </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={refreshUser}
-                  className="flex-row flex gap-2 items-center mt-3.5"
-                >
-                    <View style={{ padding: 4, borderRadius: 3 }} className="bg-[#EFEFEF1A] rounded-[10px] border-[#B3B3B333] border p-4 px-4 py-1.5">
-                        <Text className="text-center text-white font-semibold">219</Text>
-                    </View>
-                    <Text className="ml-1.5 text-xl text-grey-text font-semibold">Friends</Text>
-                </TouchableOpacity>
-            </View>
+          }
+          <View className="mt-3 text-white">
+            {
+              isLoading ? 
+              <View className="flex flex-col gap-3 mt-3">
+                <Skeleton height={20} width={"90%"}/>
+                <Skeleton width={"50%"}/>
+              </View>
+              :
+              <TouchableOpacity>
+                <Text className="text-3xl capitalize font-bold text-white">
+                  { userProfile?.name }
+                </Text>
+                <Text className="text-grey-text font-semibold">
+                  @{userProfile?.spotify_username}
+                </Text>
+              </TouchableOpacity>
+            }
 
-            <View className="mt-6">
-              <DrawerItemList {...props} /> 
-            </View>
+            <TouchableOpacity
+              onPress={refreshUser}
+              className="flex-row flex gap-2 items-center mt-3.5"
+            >
+              <View style={{ padding: 4, borderRadius: 3 }} className="bg-[#EFEFEF1A] rounded-[10px] border-[#B3B3B333] border p-4 px-4 py-1.5">
+                  <Text className="text-center text-white font-semibold">{userProfile?.friend_count || 0}</Text>
+              </View>
+              <Text className="ml-1.5 text-xl text-grey-text font-semibold">Friends</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="mt-6">
+            <DrawerItemList {...props} /> 
+          </View>
         </DrawerContentScrollView>
         <View className="pb-20 px-4 bg-[#121212]">
 
