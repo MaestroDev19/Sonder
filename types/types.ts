0f340892d996cc44e3678d4e36b8a5f186fed19e
fortune710 <fortune0208@yahoo.com>
2024-07-07 19:35:14 -0400

    added new types from DB

--- types/types.ts
+++ types/types.ts
@@ -6,17 +6,23 @@ export interface ProfileCardProps {
     avatarInitials: string;
     userName: string;
     description: string;
-    likedArtist: string[];
-    // likedGenre: string[];
-    favoriteSong: {
-        albumart: string;
-        songName: string;
-        songArtist: string;
-    };
-    favoriteArtist: {
-        albumart: string;
-        artist: string
-    }
+    likedArtist?: string[];
+    likedGenre: string[];
+    favoriteSong: FavouriteTrack;
+    favoriteArtist: FavouriteArtist,
+    onPress?: () => void
+}
+
+export interface SimilarUser {
+    artist: FavouriteArtist
+    banner: string|null,
+    bio: string,
+    id: string,
+    likes: string[],
+    name: string,
+    profile_image: string,
+    spotify_username: string,
+    track: FavouriteTrack
 }
 
 export interface User {
@@ -27,6 +33,7 @@ export interface User {
     created_at: null,
     bio: string,
     spotify_username: string,
+    friend_count: number,
     banner: string,
     country: string
 }
@@ -64,10 +71,25 @@ export interface FavouriteArtist {
     image: string
 }
 
+export interface FriendRequest {
+    sender_username: string,
+    sender_avatar: string,
+    user_id: string,
+    friend_id: string,
+    created_at: string
+}
+
 export type StorageFolder = "banners"|"avatars"|"images"
 
 
 export enum AsyncStorageKeys {
     ACCESS_TOKEN = "access-token",
     REFRESH_TOKEN = "refresh-token",
+}
+
+export enum ReactQueryKeys {
+    FRIEND_REQUESTS = "friend-requests",
+    CURRENT_USER = "current-user",
+    CURRENT_TRACK = "current-track",
+    SIMILAR_USERS = "similar-users"
 }
