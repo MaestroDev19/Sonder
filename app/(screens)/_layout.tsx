90b719fe31a2eae4bde18842eac6ef4919856af9
Charles <charles19rex@gmail.com>
2024-07-12 10:53:45 +0100

    chat is 80% done

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -1,33 +1,31 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Drawer } from 'expo-router/drawer';
-import CustomDrawer from '../../components/sidebar';
-import DrawerItems from '../../constants/DrawerItems';
-import { View, Button, Image, _View } from 'react-native';
-import { Component, useEffect } from 'react';
-import * as icons from '../../assets/svg/exports'
-import useAccessToken from '../../hooks/access-token';
-import SonderApi from '../../api';
-import { Home, MessageCircle, Settings, UserRound } from 'lucide-react-native';
-import { StatusBar } from 'expo-status-bar';
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+import CustomDrawer from "../../components/sidebar";
+import DrawerItems from "../../constants/DrawerItems";
+import { View, Button, Image, _View } from "react-native";
+import { Component, useEffect } from "react";
+import * as icons from "../../assets/svg/exports";
+import useAccessToken from "../../hooks/access-token";
+import SonderApi from "../../api";
+import { Home, MessageCircle, Settings, UserRound } from "lucide-react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
-
-  const { refreshToken } = useAccessToken()
+  const { refreshToken } = useAccessToken();
 
   useEffect(() => {
     const interceptor = SonderApi.interceptors.response.use(
-      response => {
+      (response) => {
         return response;
       },
       async (error) => {
         if (error.response && error.response.status === 401) {
           // Handle 401 error here, refresh access token
-          await refreshToken()
-          return
-        } 
+          await refreshToken();
+          return;
+        }
         return Promise.reject(error);
-      },
+      }
     );
 
     return () => {
@@ -37,15 +35,17 @@ export default function Layout() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <StatusBar style='light'/>
+      <StatusBar style="light" />
       <Drawer
-        drawerContent={props => <CustomDrawer {...props} containerStyle={{ paddingVertical: 0 }} />}
+        drawerContent={(props) => (
+          <CustomDrawer {...props} containerStyle={{ paddingVertical: 0 }} />
+        )}
         screenOptions={{
           drawerContentStyle: {
             backgroundColor: "#121212",
           },
           sceneContainerStyle: {
-            backgroundColor: "#121212"
+            backgroundColor: "#121212",
           },
           headerStyle: {
             backgroundColor: "#121212",
@@ -54,33 +54,34 @@ export default function Layout() {
           headerTitle: "",
         }}
       >
-        {DrawerItems.map(drawer => (
+        {DrawerItems.map((drawer) => (
           <Drawer.Screen
             key={drawer.name}
             name={drawer.name}
             options={{
               ...drawer.options,
               drawerIcon: () =>
-                drawer.options.title === 'Profile' ?
-                  <View className='w-9 h-9 rounded-md border-[#EFEFEF33] bg-[#EFEFEF1A] p-1.5 items-center justify-center' >
-                    <UserRound stroke="white"/>
+                drawer.options.title === "Profile" ? (
+                  <View className="w-9 h-9 rounded-md border-[#EFEFEF33] bg-[#EFEFEF1A] p-1.5 items-center justify-center">
+                    <UserRound stroke="white" />
                   </View>
-              : drawer.options.title === 'Chats' ?
-                  <View className='w-9 h-9 rounded-md border-[#EFEFEF33] bg-[#EFEFEF1A] p-1.5 items-center justify-center' >
-                    <MessageCircle stroke="white"/>
+                ) : drawer.options.title === "Chats" ? (
+                  <View className="w-9 h-9 rounded-md border-[#EFEFEF33] bg-[#EFEFEF1A] p-1.5 items-center justify-center">
+                    <MessageCircle stroke="white" />
                   </View>
-              : drawer.options.title === 'Home' ?
-                  <View className='w-9 h-9 rounded-md border-[#EFEFEF33] bg-[#EFEFEF1A] p-1.5 items-center justify-center' >
-                    <Home stroke="white"/>
+                ) : drawer.options.title === "Home" ? (
+                  <View className="w-9 h-9 rounded-md border-[#EFEFEF33] bg-[#EFEFEF1A] p-1.5 items-center justify-center">
+                    <Home stroke="white" />
                   </View>
-              : 
-                <View className='w-9 h-9 rounded-md border-[#EFEFEF33] bg-[#EFEFEF1A] p-1.5 items-center justify-center' >
-                  <Settings stroke="white"/>
-                </View>,
+                ) : (
+                  <View className="w-9 h-9 rounded-md border-[#EFEFEF33] bg-[#EFEFEF1A] p-1.5 items-center justify-center">
+                    <Settings stroke="white" />
+                  </View>
+                ),
               drawerItemStyle: {
                 borderWidth: 1,
                 marginHorizontal: 0,
-                borderColor: '#B3B3B333',
+                borderColor: "#B3B3B333",
                 borderRadius: 10,
                 padding: 5,
                 flex: 1,
@@ -90,7 +91,6 @@ export default function Layout() {
           />
         ))}
       </Drawer>
-
     </GestureHandlerRootView>
   );
 }
