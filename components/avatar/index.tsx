2c9962d0640ed86768085ab6ee7ba22e17117f28
fortune710 <fortune0208@yahoo.com>
2024-07-12 21:00:59 -0400

    added edit mode to avatar

--- components/avatar/index.tsx
+++ components/avatar/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View, Text, StyleProp, ViewStyle } from 'react-native';
+import { View, Text, StyleProp, ViewStyle, Pressable } from 'react-native';
 import styles from './styles';
 import { Image } from 'expo-image';
 import { cn } from '../../lib/utils';
+import { ImagePlus } from 'lucide-react-native';
+import { moderateScale } from 'react-native-size-matters';
 
 interface AvatarProps {
   className?: string,
@@ -10,34 +12,50 @@ interface AvatarProps {
   containerStyle?: string,
   initials: string,
   width?: number,
-  height?: number
+  height?: number,
+  editMode?: boolean,
+  onPress?: () => any
 }
 
 const Avatar = ({ src, initials, className, width, height, ...props }: AvatarProps) => {
+  function renderAvatar() {
+    return (
+      <View
+        className={cn(styles.containerStyles, props.containerStyle)}
+      >
+        {src ? (
+          <Image
+            source={{ uri: src }}
+            className={cn(styles.imageStyles, className)}
+            style={{ width: width ?? 100, height: height ?? 100, borderRadius: 999 }}
+            contentFit="fill"
+            // style={styles.image}
+          />
+        ) : (
+          <View
+            // style={styles.initials}
+            style={{ width: width ?? 100, height: height ?? 100, borderRadius: 999 }}
+            className={styles.initialsStyles}
+          > 
+              <Text className='text-3xl font-semibold mx-auto'>
+                {initials}
+              </Text>
+          </View>
+        )}
+      </View>
+    )
+  }
+  
   return (
-    <View
-      className={cn(styles.containerStyles, props.containerStyle)}
-    >
-      {src ? (
-        <Image
-          source={{ uri: src }}
-          className={cn(styles.imageStyles, className)}
-          style={{ width: width ?? 100, height: height ?? 100, borderRadius: 999 }}
-          contentFit="fill"
-          // style={styles.image}
-        />
-      ) : (
-        <View
-          // style={styles.initials}
-          style={{ width: width ?? 100, height: height ?? 100, borderRadius: 999 }}
-          className={styles.initialsStyles}
-        > 
-            <Text className='text-3xl font-semibold mx-auto'>
-              {initials}
-            </Text>
-        </View>
-      )}
-    </View>
+    <>
+      {
+        !props.editMode ? renderAvatar() :
+        <Pressable onPress={props.onPress} style={{ opacity: 0.45 }} className={cn(styles.containerStyles, "w-auto h-fit flex items-center justify-start flex-row relative")}>
+          <ImagePlus style={{ position: "absolute", zIndex: 20, left: moderateScale(25) }} stroke="white"/>
+          {renderAvatar()}
+        </Pressable>
+      }
+    </>
   );
 };
 
