84e494042c5de42ba6222f6c8dc707d4b47ec40d
fortune710 <fortune0208@yahoo.com>
2024-07-12 21:00:46 -0400

    restyled input component

--- components/input.tsx
+++ components/input.tsx
@@ -1,6 +1,7 @@
 import { Control, useController } from "react-hook-form"
 import { Text, TextInput, View } from "react-native"
 import { cn } from "../lib/utils"
+import { moderateScale } from "react-native-size-matters"
 
 interface InputProps {
     label: string,
@@ -17,12 +18,13 @@ const Input = ({ control, name, label, multiline, className }: InputProps) => {
         name
     })
     return (
-        <View className={cn("border border-light-grey rounded-lg w-full", className)}>
-            <Text className="text-xl font-semibold text-white">{label}</Text>
+        <View className={cn("border px-3 py-1 border-[#EFEFEF33] bg-[#B3B3B31A] rounded-lg w-full", className)}>
+            <Text style={{ fontSize: moderateScale(12) }} className="font-semibold text-white">{label}</Text>
             <TextInput
                 value={field.value}
                 onChangeText={field.onChange}
                 multiline={multiline}
+                className="my-1.5 text-white"
             />
         </View>
     )
