6c302b2b59188d006d9a8a303c1db196ce97255b
fortune710 <fortune0208@yahoo.com>
2024-07-16 20:58:12 -0400

    funtion for relative date

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,3 +1,21 @@
+import { formatDistanceToNowStrict, isToday, isYesterday, format } from 'date-fns';
+
+export function formatRelativeDate(date: Date): string {
+  if (isToday(date)) {
+    const distance = formatDistanceToNowStrict(date, { addSuffix: false });
+    if (distance.includes('second')) return 'now';
+    return distance.replace(' minutes', 'm')
+                   .replace(' hours', 'h')
+                   .replace(' hour', 'h');
+  } else if (isYesterday(date)) {
+    return 'Yesterday';
+  } else if (date.getFullYear() === new Date().getFullYear()) {
+    return format(date, 'MMM d');
+  } else {
+    return format(date, 'MMM d, yyyy');
+  }
+}
+
 export function millisecondsToMSFormat(milliseconds: number): string {
     // Calculate minutes and seconds from milliseconds
     const minutes = Math.floor(milliseconds / (1000 * 60)); // Minutes
