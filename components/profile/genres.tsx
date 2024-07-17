ee1ae71d5f9103c5473d9ae360766f760baad92f
fortune710 <fortune0208@yahoo.com>
2024-07-16 21:22:06 -0400

    fixed styling

--- components/profile/genres.tsx
+++ components/profile/genres.tsx
@@ -7,36 +7,38 @@ const FavouriteGenresTab = () => {
     const { isLoading, favouriteGenres } = useFavouriteGenres();
 
     return (
-        <ScrollView className="border border-[#EFEFEF4A] p-4 rounded-lg" style={{ flex: 1, height: "100%" }} contentContainerClassName="pb-56">
-            {
-                isLoading ? 
-                <>
-                    {
-                        Array.from({ length: 6 }).map((_, index) => (
-                            <Skeleton 
-                                style={{ marginBottom: verticalScale(10) }} 
-                                key={index} 
-                                height={verticalScale(60)} 
-                                width="100%"
-                            />
-                        ))
-                    }
-                </>
-                
-                :
-                favouriteGenres?.map((genre, index) => (
-                <View className="bg-[#B3B3B31A] rounded-md flex flex-row gap-4 items-center p-3 border border-[#EFEFEF33] mb-4" key={genre}>
-                    <Text className="text-white py-4 px-5 font-bold rounded-md border border-[#EFEFEF4A] bg-[#B3B3B31A]">
-                        {index + 1}
-                    </Text>
-                    <View>
-                        <Text className="text-white capitalize text-xl font-semibold whitespace-break-spaces">{genre}</Text>
+        <View style={{ height: "75%" }}>
+            <ScrollView className="border border-[#EFEFEF4A] p-4 rounded-lg" style={{ flex: 1, height: "100%" }}>
+                {
+                    isLoading ? 
+                    <>
+                        {
+                            Array.from({ length: 6 }).map((_, index) => (
+                                <Skeleton 
+                                    style={{ marginBottom: verticalScale(10) }} 
+                                    key={index} 
+                                    height={verticalScale(60)} 
+                                    width="100%"
+                                />
+                            ))
+                        }
+                    </>
+                    
+                    :
+                    favouriteGenres?.map((genre, index) => (
+                    <View className="bg-[#B3B3B31A] rounded-md flex flex-row gap-4 items-center p-3 border border-[#EFEFEF33] mb-4" key={genre}>
+                        <Text className="text-white py-4 px-5 font-bold rounded-md border border-[#EFEFEF4A] bg-[#B3B3B31A]">
+                            {index + 1}
+                        </Text>
+                        <View>
+                            <Text className="text-white capitalize text-xl font-semibold whitespace-break-spaces">{genre}</Text>
+                        </View>
                     </View>
-                </View>
-                ))
-            }
+                    ))
+                }
 
-        </ScrollView>
+            </ScrollView>
+        </View>
     )
 };
 
--- components/profile/songs.tsx
+++ components/profile/songs.tsx
@@ -7,37 +7,40 @@ import { Image } from "expo-image";
 const FavoriteSongsTab = () => {
     const { isLoading, favouriteSongs } = useFavouriteSongs();
     return (
-        <ScrollView className="border border-[#EFEFEF4A] p-4 rounded-lg" style={{ flex: 1, height: "80%" }} contentContainerClassName="pb-56">
-            {
-                isLoading ? 
-                <>
-                    {
-                        Array.from({ length: 6 }).map((_, index) => (
-                            <Skeleton 
-                                style={{ marginBottom: verticalScale(10) }} 
-                                key={index} 
-                                height={verticalScale(60)} 
-                                width="100%"
-                            />
-                        ))
-                    }
-                </>
-                :
-                favouriteSongs?.map((track) => (
-                <View className="bg-[#B3B3B31A] rounded-md flex flex-row gap-4 items-center p-3 border border-[#EFEFEF33] mb-4" key={track.id}>
-                    <Text className="text-white py-4 px-5 font-bold rounded-md border border-[#EFEFEF4A] bg-[#B3B3B31A]">
-                        {track?.position}
-                    </Text>
-                    <Image source={{ uri: track?.image }} style={{ width: scale(50), height: verticalScale(50), borderRadius: 6 }}/>
-                    <View className="w-[60%] flex flex-col gap-0.5">
-                        <Text className="text-white text-xl font-semibold whitespace-break-spaces">{track.name}</Text>
-                        <Text className="text-light-grey">{track.artists.map((artist) => artist.name).join(', ')}</Text>
+        <View style={{ height: "75%" }}>
+            <ScrollView className="border border-[#EFEFEF4A] p-4 rounded-lg" style={{ flex: 1, height: "80%" }}>
+                {
+                    isLoading ? 
+                    <>
+                        {
+                            Array.from({ length: 6 }).map((_, index) => (
+                                <Skeleton 
+                                    style={{ marginBottom: verticalScale(10) }} 
+                                    key={index} 
+                                    height={verticalScale(60)} 
+                                    width="100%"
+                                />
+                            ))
+                        }
+                    </>
+                    :
+                    favouriteSongs?.map((track) => (
+                    <View className="bg-[#B3B3B31A] rounded-md flex flex-row gap-4 items-center p-3 border border-[#EFEFEF33] mb-4" key={track.id}>
+                        <Text className="text-white py-4 px-5 font-bold rounded-md border border-[#EFEFEF4A] bg-[#B3B3B31A]">
+                            {track?.position}
+                        </Text>
+                        <Image source={{ uri: track?.image }} style={{ width: scale(50), height: verticalScale(50), borderRadius: 6 }}/>
+                        <View className="w-[60%] flex flex-col gap-0.5">
+                            <Text className="text-white text-xl font-semibold whitespace-break-spaces">{track.name}</Text>
+                            <Text className="text-light-grey">{track.artists.map((artist) => artist.name).join(', ')}</Text>
+                        </View>
                     </View>
-                </View>
-                ))
-            }
+                    ))
+                }
 
-        </ScrollView>
+            </ScrollView>
+
+        </View>
     )
 };
 
