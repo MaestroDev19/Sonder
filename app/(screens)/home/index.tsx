afc1ea8b242e32139af8c7675d11a3003dbec38b
fortune710 <fortune0208@yahoo.com>
2024-07-20 21:33:06 -0400

    changed home structure

--- app/(screens)/home/index.tsx
+++ app/(screens)/home/index.tsx
@@ -1,40 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
-import { getHomeNavbarOptions } from '../../../components/navBar';
-import NowPlaying from '../../../components/nowPlaying';
-import { container } from './styles';
-import { Drawer } from 'expo-router/drawer';
-import useCurrentTrack from '../../../hooks/current-song';
-import { SwipeCardsContainer } from '../../../components/swipeCardsContainer';
-import { millisecondsToMSFormat } from '../../../utils/functions';
+import { HomePageLayout } from '../../../components/home';
+
 //import useCurrentUser from '../../../hooks/current-user';
 
 
 
 const HomeScreen: React.FC = () => {
-  const { isLoading, currentTrack, currentTrackProgress } = useCurrentTrack();
-  //const { refreshUser } = useCurrentUser()
-
-  return (
-    <View className={container}>
-      <Drawer.Screen options={getHomeNavbarOptions()} />
-
-      <View className='px-4'>
-        <NowPlaying
-          isLoading={isLoading}
-          songName={currentTrack?.name || "--"}
-          albumArtUrl={currentTrack?.image || 'https://upload.wikimedia.org/wikipedia/en/3/32/Frank_Ocean-Nostalgia_Ultra.jpeg'} //Change default to green image
-          artist={currentTrack?.artists.join(', ') || '--'}
-          timestamp={millisecondsToMSFormat(currentTrackProgress)}
-          device={currentTrack?.device.name || "N/A"}
-          deviceType={currentTrack?.device.type || "Computer"}
-        />
-      </View>
-
-      <SwipeCardsContainer/>
-
-    </View>
-  );
+  return <HomePageLayout/>
 };
 
 export default HomeScreen;
